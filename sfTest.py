ac4f3375b75db72d4ff79eb3907777bacf212b73
Yucheng Wang <kris_7z@hotmail.com>
2022-05-13 13:58:44 -0700

    Update sfTest.py

--- sfTest.py
+++ sfTest.py
@@ -9,7 +9,7 @@ from sys import maxint,argv
 if argv[1:]: s=int(argv[1])
 else:
 	s = randint(0, maxint )
-	print 'seed:\t%i'%s
+	print('seed:\t%i'%s)
 seed(s)
 
 polies,verts= TaxiVoronoi( RandomPointGrid( gridSize=8 , noise=1.0 ) )
